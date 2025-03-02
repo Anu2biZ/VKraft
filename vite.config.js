1a50d51f36b54d89f04fdfe85fb7f390636f46a5
anu2biz <anu2biz@yandex.ru>
2025-03-03 01:54:39 +0300

    Remove dist

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   root: 'web',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true
-  },
   server: {
     port: 3000,
     proxy: {
